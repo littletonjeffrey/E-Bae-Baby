7d66eb16f84480c04513aa21cb3f1b2eeb3f963d
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-13 15:31:12 -0500

    set up models and index.js in models

---
+++ Develop/models/index.js
@@ -0,0 +1,39 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // When we delete a Driver, make sure to also delete the associated License.
+  // onDelete: 'CASCADE',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag
+  }
+})
+// Tags belongToMany Products (through ProductTag)
+ Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag
+  }
+})
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
