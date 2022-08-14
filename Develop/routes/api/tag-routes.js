7413b4c227854d03ceb6550a8287f00e754d20f1
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-14 00:03:29 -0500

    tag routes for get alltags, get tag by pk, and post tag are created and tested

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,18 +5,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
   // be sure to include its associated Product data
+  include: [Product],
+  })
+  .then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    // be sure to include its associated Product data
+    include: [Product]
+    })
+  .then (data => {
+    res.json(data)
+  })  .catch (err => {
+    res.status(500).json(err);
+  })
+  
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+     res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
