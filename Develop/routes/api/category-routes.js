c19bef54bcc27aa31e50788b902ed2b0df0f1324
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-13 23:21:23 -0500

    category delete route completed and tested

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -39,20 +39,34 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
   try {
+    //fixed syntax on this call by referencing Kurt Bixby's challenge
+    // =============================
     res.status(200).json(await Category.update(req.body,{
-      where: {
-        id: req.params.id}
+      where: {id: req.params.id}
       }));
-   
+  //  ================================
     
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
