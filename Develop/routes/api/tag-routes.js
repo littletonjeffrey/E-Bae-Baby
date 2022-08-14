5d4ec855ef3a5f6f96bc8d5d7812f27e14ea9b6c
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-14 00:07:53 -0500

    tag put and delete routes created and tested

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -39,12 +39,35 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+   
+    res.status(200).json(await Tag.update(req.body,{
+      where: {id: req.params.id}
+      }));
+  
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
